5ff7969f717d281c30fa2391a4e9706fc7c18a08
AnwarDabbour <Anwar95github>
2019-10-12 15:13:11 +0300

    hydration goal - giving notifications to user to drink. day 8:00 - 20:00 and notification is sent every 15 minutes according to the bottle in watter state.

--- app/src/main/java/com/example/bottle/MainActivity.java
+++ app/src/main/java/com/example/bottle/MainActivity.java
@@ -25,14 +25,24 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     public static final String NOTIFICATION_CHANNEL_ID = "channel_id";
     public static final int NOTIFICATION_ID = 101;
+//safe to drink
     private static int allowed_days_drink =4;
-    public static final String CHANNEL_NAME = "Notification Channel";
-    int importance = NotificationManager.IMPORTANCE_DEFAULT;
-    private static final String APP_ID = "q1XJyymP9b7EfWjZjzuCQTrsmLmaPUmXKNt/Jq5EorXltx8yYQ1Dag==";
+//days,hours,minutes
     float level_past = -1;
     int day_past = -1;// the last day the person drank in it
+    int minutes_past=-1;
     SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
     SharedPreferences.Editor editor = pref.edit();
+//hydration goal
+    private static int allowed_minutes_notification =15;
+    private static double  cup_litters=0.25;
+    private static int goal_littersPerDay=2;
+    private int drank_littersPerDay=0;
+//notifcation
+    public static final String CHANNEL_NAME = "Notification Channel";
+    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//function apps
+    private static final String APP_ID = "q1XJyymP9b7EfWjZjzuCQTrsmLmaPUmXKNt/Jq5EorXltx8yYQ1Dag==";
 
     public MainActivity() throws IOException {
     }
@@ -55,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         float level = (float)level_double; // 0 <= level <= 1
         text_level.setText("Water Level: " + level*100 + "%"); //set text for text view
 
-        //if hot 40+, mraq 4 ayam mt3abatesh
         Calendar cal = Calendar.getInstance();
         int day = cal.get(Calendar.DAY_OF_MONTH);
         if(day_past == -1){
@@ -71,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         if(temperature_double > 40){
             text_expire.setText("Don't Drink - Refill it again!!"); //set text for text view
         }else {
-            if (level <= level_past && day >= day_past +  allowed_days_drink) { // not the same data > 4
+            if (level <= level_past && day >= (day_past +  allowed_days_drink)) { // not the same data > 4
                     text_expire.setText("Don't Drink - Refill it again!!"); //set text for text view
 
             } else {
@@ -79,14 +88,38 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         if(level > level_past){
-            update_level(level);
             update_day(day);
+
         }
 
+
+        int hours=cal.get(Calendar.HOUR);
+        int minutes= cal.get(Calendar.MINUTE);
+        int left_minutes_in_halfday = (60 - minutes) + (20 - hours)*60;
+        pref.getInt("minutes_past",minutes_past);
+        double number_of_notifications=(goal_littersPerDay - drank_littersPerDay)/(cup_litters * left_minutes_in_halfday);
+        //allowing notfication every 15 minutes from 8:00 am to 20pm
+        if(number_of_notifications > 0 &&
+                minutes>=minutes_past+allowed_minutes_notification &&
+                8 <= hours && hours <=20
+         ){
+            sendNotification();
+            update_minutes(minutes);
+        }
+
+        if(level < level_past){
+            update_minutes(minutes);
+        }
+        if(level != level_past){
+            update_level(level);
+        }
+
+
+        //notification
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, CHANNEL_NAME, importance);
 
-//Boolean value to set if lights are enabled for Notifications from this Channel
+            //Boolean value to set if lights are enabled for Notifications from this Channel
             notificationChannel.enableLights(true);
             //Boolean value to set if vibration are enabled for Notifications from this Channel
             notificationChannel.enableVibration(true);
@@ -116,17 +149,8 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.textView2);
         new GetWeatherTask(textView).execute(url);
 
-        Button b1 = (Button)findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendNotification();
-            }
-        });
 
-
-
-    }
+        }
 
     private void sendNotification(){
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
@@ -154,17 +178,21 @@ public class MainActivity extends AppCompatActivity {
 
     private void update_level(float level) {
         level_past = level;
-        editor.putFloat("level_past", level_past); // Storing long
+        editor.putFloat("level_past", level_past); // Storing Float
         editor.commit(); // commit changes
     }
     private void update_day(int day) {
         day_past = day;
-        editor.putFloat("day_past", day_past); // Storing long
+        editor.putInt("day_past", day_past); // Storing Int
+        editor.commit(); // commit changes
+    }
+    private void update_minutes(int minutes) {
+        minutes_past = minutes;
+        editor.putInt("minutes_past", minutes_past); // Storing Int
         editor.commit(); // commit changes
     }
 
 
-        String name_lina=" This is a new branch test lina";
 
 
 
