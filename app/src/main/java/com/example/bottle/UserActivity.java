8e97e54c82a5bdbc5a501a886abfc2bec4e5f7ff
AnwarDabbour <Anwar95github>
2019-10-20 20:27:05 +0300

    user activity + sign up

--- app/src/main/java/com/example/bottle/UserActivity.java
+++ app/src/main/java/com/example/bottle/UserActivity.java
@@ -2,13 +2,63 @@ package com.example.bottle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserActivity extends AppCompatActivity {
+    Button login, signup;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        login = (Button) findViewById(R.id.btnlogin);
+        signup = (Button) findViewById(R.id.btnsignup);
+        email = (EditText) findViewById(R.id.Email);
+        password = (EditText) findViewById(R.id.Password);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup();
+            }
+        });
     }
-}
+        public void login(){
+            String user =email.getText().toString().trim();
+            String pass = password.getText().toString().trim();
+            if(user.equals("123@gmail.com") && pass.equals("123")){
+                Toast.makeText(this,"Email and Password matched!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(UserActivity.this, MainActivity.class);
+                startActivity(intent);
+            }else{
+                Toast.makeText(this,"Email and Password do not match!", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    public void signup(){
+        Intent intent = new Intent(UserActivity.this, SignupActivity.class);
+        startActivity(intent);
+//        String user =email.getText().toString().trim();
+//        String pass = password.getText().toString().trim();
+//        if(user.equals("123@gmail.com") && pass.equals("123")){
+//            Toast.makeText(this,"Email and Password matched!", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(UserActivity.this, MainActivity.class);
+//            startActivity(intent);
+//        }else{
+//            Toast.makeText(this,"Email and Password do not match!", Toast.LENGTH_LONG).show();
+//        }
+    }
+    }
+
